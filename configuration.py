64df631ab8106989cae35cb3fc5945e6f3e96d11
Momilijaz96 <momilijaz@gmail.com>
2022-02-26 20:57:08 -0500

    updated configuration

--- configuration.py
+++ configuration.py
@@ -1,11 +1,11 @@
 def build_config(dataset):
     cfg = type('', (), {})()
     if dataset == 'TinyVirat':
-        cfg.data_folder = '/home/mijaz/TinyAction/datasets/TinyVIRAT-v2/videos'
-        cfg.train_annotations = '/home/mijaz/TinyAction/datasets/TinyVIRAT-v2/tiny_train_v2.json'
-        cfg.val_annotations = '/home/mijaz/TinyActiondatasets/TinyVIRAT-v2/tiny_val_v2.json'
-        cfg.test_annotations = '/home/mijaz/TinyAction/datasets/TinyVIRAT-v2/tiny_test_v2_public.json'
-        cfg.class_map = '/home/mijaz/TinyAction/datasets/TinyVIRAT-v2/class_map.json'
+        cfg.data_folder = '/home/mijaz/TinyActions/datasets/TinyVIRAT-v2/videos'
+        cfg.train_annotations = '/home/mijaz/TinyActions/datasets/TinyVIRAT-v2/tiny_train_v2.json'
+        cfg.val_annotations = '/home/mijaz/TinyActions/datasets/TinyVIRAT-v2/tiny_val_v2.json'
+        cfg.test_annotations = '/home/mijaz/TinyActions/datasets/TinyVIRAT-v2/tiny_test_v2_public.json'
+        cfg.class_map = '/home/mijaz/TinyActions/datasets/TinyVIRAT-v2/class_map.json'
         cfg.num_classes = 26
     #cfg.saved_models_dir = './results/saved_models'
     #cfg.tf_logs_dir = './results/logs'
