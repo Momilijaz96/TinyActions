2b4413272978aac572221d6f2ee9b7ab90ded737
Momilijaz96 <momilijaz@gmail.com>
2022-03-29 17:49:37 -0400

    added countframes

---
+++ utils/countframes.py
@@ -0,0 +1,22 @@
+from imutils.video import count_frames
+import os
+import matplotlib.pyplot as plt
+
+# count the total number of frames in the video file
+path = 'TinyVIRAT_v2/videos/train'
+
+frame_count=[]
+# display the frame count to the terminal
+for x in os.walk(path):
+    for f in x[2]:
+        total = count_frames(path, override=True)
+        print("Total frames: ",total)
+        frame_count.append(total)
+
+#Get frame count distribution plot
+plt.figure()
+plt.plot(frame_count)
+plt.ylabel("Frame count")
+plt.xlabel("Frequency")
+plt.title("TinyVIRAT-v2 training Framecount distribution")
+plt.savefig('framecount.png')
