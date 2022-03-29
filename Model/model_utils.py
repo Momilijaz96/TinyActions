2ce8af8918c9873f4569b65c5c5047a09deab06c
Momal Ijaz <53872365+Momilijaz96@users.noreply.github.com>
2022-03-29 15:57:07 -0400

    Added perceiver block

--- Model/model_utils.py
+++ Model/model_utils.py
@@ -158,3 +158,30 @@ class Block(nn.Module):
         x = xq + self.drop_path(self.cross_attn(xq,xkv))
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
+    
+    
+#Perceiver Block
+class PBlock(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+            drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.cross_attn=CVAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, 
+                attn_drop=attn_drop, proj_drop=drop)
+
+    def forward(self, xq, xkv):
+        #Cross Attention block
+        x = xq + self.drop_path(self.cross_attn(xq,xkv)) 
+        
+        #Latent Transformer
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        
+        return x
