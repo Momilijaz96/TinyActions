b9e3886bce378ae7d95cc882a9f34a04a62b1028
Momilijaz96 <momilijaz@gmail.com>
2022-04-12 12:13:19 -0400

    window size

--- Model/VideoSWIN.py
+++ Model/VideoSWIN.py
@@ -481,7 +481,7 @@ class SwinTransformer3D(nn.Module):
                  embed_dim=96,
                  depths=[2, 2, 6, 2],
                  num_heads=[3, 6, 12, 24],
-                 window_size=(2,7,7),
+                 window_size=(8,7,7),
                  mlp_ratio=4.,
                  qkv_bias=True,
                  qk_scale=None,
