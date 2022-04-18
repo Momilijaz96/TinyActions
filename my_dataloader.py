b92401b04e7018435407b7fdae1ead8485b5f0cc
Momilijaz96 <momilijaz@gmail.com>
2022-04-18 18:48:39 -0400

    Update my_dataloader.py

--- my_dataloader.py
+++ my_dataloader.py
@@ -89,7 +89,7 @@ class TinyVIRAT_dataset(Dataset):
             tiled=np.tile(last_frame,(diff,1,1,1))
             frames=np.append(frames,tiled,axis=0) 
         frames = torch.from_numpy(frames)
-        clips = self.transform(x)
+        clips = self.transform(frames)
         return clips
 
     def __len__(self):
